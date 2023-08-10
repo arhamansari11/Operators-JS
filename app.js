289e4601c8558c3099f70a42c8f67b3e88693483
Muhammad Arham <124850772+arhamansari11@users.noreply.github.com>
2023-08-10 14:30:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+// 1 => Arithemetic Operators
+
+let a = 14;
+let b = 2;
+console.log(a + b);
+console.log(a -  b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log(++a)
+console.log(a++)
+console.log(--a)
+console.log(a--)
+
+console.clear()
+
+
+// 2 => Logical Operators
+
+let mybool = !false
+console.log(mybool)   // ! Not Operator
+
+let aa = 10;
+let bb = 20;
+console.log(bb=aa && aa===bb)  // && And operator
+// console.clear();                        
+
+
+let s = 20;
+let d = "20";
+console.log(s>d || s==d)   //  || Or Operator
+
+ console.clear();
+
+// 3 => Condition Operator
+
+// let x = 10;
+// let y = "20";
+
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x <= y)
+// console.log(x >= y)
+// console.log(x == y)
+
+
+// console.clear();
+
+
+// 4 => Assignment Operators
+
+// let assignment = 20;
+// assignment += 5;
+// console.log(assignment)
+
+// assignment -= 5;
+// console.log(assignment)
+
+// assignment *= 5;
+// console.log(assignment)
+
+// assignment /= 2;
+// console.log(assignment)
+
+// assignment %= 3;
+// console.log(assignment)
+
+// assignment **= 2;
+// console.log(assignment)
